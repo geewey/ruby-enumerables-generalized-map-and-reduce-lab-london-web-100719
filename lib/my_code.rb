a6e06bbf945aef870c8fdcbaab0c876be90468df
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-09-26 16:03:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,34 +10,21 @@ def map(source_array)
 
   return new_array
 end
-
-
-##  aggregator = true
-# returns true when all values are truthy
-# returns true when a truthy value is present
-
-##  aggregator = false
-# returns false when any value is false
-# returns false when no truthy value is present
   
 
-def reduce(source_array, starting_point=0)
-  aggregator = source_array[0]
-  i = 0
+def reduce(source_array, starting_point=nil)
+  if starting_point
+    aggregator = starting_point
+    i = 0
+  else
+    aggregator = source_array[0]
+    i = 1
+  end
+
   while i < source_array.length
-    yield(source_array, starting_point)  
-    
-    # # returns a running total when not given a starting point
-    # if (source_array[i] is integer)
-    #   aggregator = 0
-    #   while i < source_array.length do
-    #     aggregator += source_array[i]
-    #     i += 1
-    #   end
-    # end
-    
+    aggregator = yield(aggregator, source_array[i]) # returns a single value
     i += 1
   end
-  
+
   return aggregator
 end
